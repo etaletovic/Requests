1a88308552bf176a9bd5ce9ee4fdaff0cdd95fa2
emt <emir.taletovic@gmail.com>
2020-01-20 15:50:31 -0800

    Can hit endpoints and basic tests passing

---
+++ src/Requests/Requests.swift
@@ -0,0 +1,66 @@
+//
+//  Requests.swift
+//  Requests
+//
+//  Created by Emir Taletovic on 1/20/20.
+//  Copyright © 2020 Emir Taletovic. All rights reserved.
+//
+import Foundation
+
+public struct Requests {
+    
+    static var session: URLSession = URLSession(configuration: .default, delegate: nil, delegateQueue:  OperationQueue())
+    
+    public static func get(url: URL, timeoutInSeconds: Double = 15) -> Result<String?, NetworkError> {
+        
+        request(url: url, body: nil, httpMethod: "GET", timeoutInSeconds: timeoutInSeconds)
+    }
+    
+    public static func post(url: URL, body: Data?, timeoutInSeconds: Double = 15) -> Result<String?, NetworkError> {
+        
+        request(url: url, body: body, httpMethod: "POST", timeoutInSeconds: timeoutInSeconds)
+    }
+    
+    public static func put(url: URL, body: Data?, timeoutInSeconds: Double = 15) -> Result<String?, NetworkError> {
+        
+        request(url: url, body: body, httpMethod: "PUT", timeoutInSeconds: timeoutInSeconds)
+    }
+    
+    public static func delete(url: URL, timeoutInSeconds: Double = 15) -> Result<String?, NetworkError> {
+        request(url: url, body: nil, httpMethod: "DELETE", timeoutInSeconds: timeoutInSeconds)
+    }
+    
+    public static func request(url: URL, body: Data?, httpMethod: String, timeoutInSeconds: Double) -> Result<String?, NetworkError> {
+        
+        var urlRequest = URLRequest(url: url, timeoutInterval: timeoutInSeconds)
+        urlRequest.httpBody = body
+        urlRequest.httpMethod = httpMethod
+                
+        return request(urlRequest: urlRequest, body: body, httpMethod: httpMethod, timeoutInSeconds: timeoutInSeconds)
+    }
+    
+    public static func request(urlRequest: URLRequest, body: Data?, httpMethod: String, timeoutInSeconds: Double) -> Result<String?, NetworkError> {
+        
+        var result: Result<String?, NetworkError>!
+        let semaphore = DispatchSemaphore(value: 0)
+        let dt = session.dataTask(with: urlRequest) { (data, _, _) in
+            if let data = data {
+                result = .success(String(data: data, encoding: .utf8))
+            } else {
+                result = .failure(.server)
+            }
+            semaphore.signal()
+        }
+        dt.resume()
+        
+        _ = semaphore.wait(wallTimeout: .distantFuture)
+        
+        return result
+    }
+    
+    public enum NetworkError: Error {
+        case url
+        case server
+        case timeout
+    }
+}
---
+++ src/Tests/RequestsTests.swift
@@ -0,0 +1,91 @@
+//
+//  Tests.swift
+//  Tests
+//
+//  Created by Emir Taletovic on 1/20/20.
+//  Copyright © 2020 Emir Taletovic. All rights reserved.
+//
+
+import XCTest
+@testable import Requests
+class RequestsTests: XCTestCase {
+
+    let apiUrl = ""
+    let jsonEncoder = JSONEncoder()
+    
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testGet() {
+                
+        let url = URL(string: apiUrl)!
+        
+        let res = try? Requests.get(url: url).get()
+        
+        XCTAssert(res != nil)
+    }
+    
+    func testPost() {
+        
+        let url = URL(string: apiUrl)!
+        
+        let member = Person (
+            name: "Mujo",
+            age: 25,
+            children: [
+                Person(name: "Stuka", age: 13, children: nil),
+                Person(name: "Hanka", age: 30, children: nil)
+            ]
+        )
+        
+        let data = try? jsonEncoder.encode(member)
+        
+        XCTAssert(data != nil)
+
+        let res = try? Requests.post(url: url, body: data).get()
+        
+        XCTAssert(res != nil)
+    }
+    
+    func testPut() {
+        
+        let url = URL(string: apiUrl)!
+        
+        let member = Person (
+            name: "Mujo",
+            age: 25,
+            children: [
+                Person(name: "Stuka", age: 13, children: nil),
+                Person(name: "Hanka", age: 30, children: nil)
+            ]
+        )
+        
+        let data = try? jsonEncoder.encode(member)
+        
+        XCTAssert(data != nil)
+
+        let res = try? Requests.put(url: url, body: data).get()
+        
+        XCTAssert(res != nil)
+    }
+    
+    func testDelete() {
+        
+        let url = URL(string: apiUrl)!
+        
+        let res = try? Requests.delete(url: url).get()
+        
+        XCTAssert(res != nil)
+    }
+}
+
+struct Person: Codable {
+    let name: String
+    let age: Int
+    let children: [Person]?
+}
